56f03530c830faa7fb71193c7aa3f285cdb162bd
Ces Araujo <ces@macbookpro-ces-araujo-2.local>
2016-05-30 18:09:49 -0500

    Initial Commit

---
+++ Coursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coursera
+//
+//  Created by Ces Araujo on 30/05/16.
+//  Copyright © 2016 Ulises Roldan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
